b53472c8afe1696971e98b040fe5ab1941707ff2
Сергей <040620@mail.ru>
2021-12-08 18:24:46 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringcollections/SkyProSpringCollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringcollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProSpringCollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProSpringCollectionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/skyprospringcollections/SkyProSpringCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringcollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProSpringCollectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
